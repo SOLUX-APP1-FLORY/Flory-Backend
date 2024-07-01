1b3e43a217fc1a33e0569395fa141edef4308c64
ouob123 <kyjin320@sookmyung.ac.kr>
2024-07-01 22:30:31 +0900

    프로젝트 초기 세팅

---
+++ src/main/java/flory/FloryServer/FloryServerApplication.java
@@ -0,0 +1,13 @@
+package flory.FloryServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FloryServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/flory/FloryServer/FloryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package flory.FloryServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
