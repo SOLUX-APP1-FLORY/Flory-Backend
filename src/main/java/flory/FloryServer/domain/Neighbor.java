05bd0fffc31174eee689a9bffe167e0188a328c3
ouob123 <kyjin320@sookmyung.ac.kr>
2024-07-18 16:25:56 +0900

    Feat: Entity 설계

---
+++ src/main/java/flory/FloryServer/domain/Neighbor.java
@@ -0,0 +1,24 @@
+package flory.FloryServer.domain;
+
+import flory.FloryServer.domain.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Neighbor extends BaseEntity {
+
+    @Id
+    // 기본 키 생성
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToMany(mappedBy = "neighbor", cascade = CascadeType.ALL)
+    private List<Relationship> relationshipList = new ArrayList<>();
+}
