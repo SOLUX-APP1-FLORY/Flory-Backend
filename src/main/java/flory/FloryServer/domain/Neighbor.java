0c59d72c3a1d3555bc94a4239efafe758d7b6cc2
ouob123 <132811106+ouob123@users.noreply.github.com>
2024-07-18 23:26:08 +0900

    Update Neighbor.java

--- src/main/java/flory/FloryServer/domain/Neighbor.java
+++ src/main/java/flory/FloryServer/domain/Neighbor.java
@@ -15,7 +15,8 @@ import java.util.List;
 public class Neighbor extends BaseEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY) // 기본 키 생성
+    // 기본 키 생성
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @OneToMany(mappedBy = "neighbor", cascade = CascadeType.ALL)
