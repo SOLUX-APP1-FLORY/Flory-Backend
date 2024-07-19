93d0a88c38f0e401ddf57699adfba749fde52c40
lehiewl <2110801@sookmyung.ac.kr>
2024-07-20 00:34:28 +0900

    conflict resolve

--- src/main/java/flory/FloryServer/domain/base/BaseEntity.java
+++ src/main/java/flory/FloryServer/domain/base/BaseEntity.java
@@ -1,8 +1,4 @@
-<<<<<<<< HEAD:src/main/java/flory/FloryServer/domain/common/BaseEntity.java
-package flory.FloryServer.domain.common;
-========
 package flory.FloryServer.domain.base;
->>>>>>>> dbca207f12e8cb0a661181084bd16bb4013b93b5:src/main/java/flory/FloryServer/domain/base/BaseEntity.java
 
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
--- src/main/java/flory/FloryServer/service/UserService.java
+++ src/main/java/flory/FloryServer/service/UserService.java
@@ -6,19 +6,19 @@ import flory.FloryServer.repository.UserRepository;
 import flory.FloryServer.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
 
-@Service
-public class UserService {
-    private final UserRepository memberRepository;
-
-    @Autowired
-    public UserService(UserRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
-
-    @Transactional
-    public void saveNickname(String nickname) {
-        User newMember = new User();
-        newMember.setNickname(nickname);
-        memberRepository.save(newMember);
-    }
-}
+//@Service
+//public class UserService {
+//    private final UserRepository memberRepository;
+//
+//    @Autowired
+//    public UserService(UserRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
+//
+//    @Transactional
+//    public void saveNickname(String nickname) {
+//        User newMember = new User();
+//        newMember.setNickname(nickname);
+//        memberRepository.save(newMember);
+//    }
+//}
