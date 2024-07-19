b252d9e04020fb153b83b717ff9d2cf3262ce0e7
lehiewl <2110801@sookmyung.ac.kr>
2024-07-20 00:34:28 +0900

    conflict resolve

--- src/main/java/flory/FloryServer/controller/UserController.java
+++ src/main/java/flory/FloryServer/controller/UserController.java
@@ -10,31 +10,31 @@ import flory.FloryServer.repository.UserRepository;
 
 import java.util.Optional;
 
-@RestController
-@RequestMapping("/api/v1")
-public class UserController {
-    private final UserRepository userRepository;
-
-    @Autowired
-    public UserController(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
-    @PostMapping("/member")
-    public ResponseEntity<String> saveNickname(@RequestBody UserUpdateRequest request) {
-        try {
-            Optional<User> userOptional = userRepository.findById(request.getId());
-            if(userOptional.isPresent()){
-                User user = userOptional.get();
-                user.updateProfile(request.getNickname(), request.getGender());
-                userRepository.save(user);
-                return ResponseEntity.ok("Profile updated successfully");
-            } else {
-                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
-            }
-
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to save nickname");
-        }
-    }
-}
+//@RestController
+//@RequestMapping("/api/v1")
+//public class UserController {
+//    private final UserRepository userRepository;
+//
+//    @Autowired
+//    public UserController(UserRepository userRepository) {
+//        this.userRepository = userRepository;
+//    }
+//
+//    @PostMapping("/member")
+//    public ResponseEntity<String> saveNickname(@RequestBody UserUpdateRequest request) {
+//        try {
+//            Optional<User> userOptional = userRepository.findById(request.getId());
+//            if(userOptional.isPresent()){
+//                User user = userOptional.get();
+////                user.updateProfile(request.getNickname(), request.getGender());
+//                userRepository.save(user);
+//                return ResponseEntity.ok("Profile updated successfully");
+//            } else {
+//                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+//            }
+//
+//        } catch (Exception e) {
+//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to save nickname");
+//        }
+//    }
+//}
