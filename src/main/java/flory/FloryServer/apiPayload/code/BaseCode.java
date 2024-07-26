9694b0fb5b98dacaf5e9438b780186084b026bf4
jisu jang <wltnslagg@naver.com>
2024-07-27 01:00:56 +0900

    Feat: API 응답 통일 연습

---
+++ src/main/java/flory/FloryServer/apiPayload/code/BaseCode.java
@@ -0,0 +1,8 @@
+package flory.FloryServer.apiPayload.code;
+
+public interface BaseCode {
+
+    public ReasonDTO getReason();   //Reason-> ReasonDTO
+
+    public ReasonDTO getReasonHttpStatus();
+}
