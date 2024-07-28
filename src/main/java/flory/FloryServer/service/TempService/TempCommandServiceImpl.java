671863f0b12a66cacbbee411d59281c5aa432172
jisu jang <wltnslagg@naver.com>
2024-07-29 00:34:04 +0900

    Style: TempCommandService 주석처리 설명

--- src/main/java/flory/FloryServer/service/TempService/TempCommandServiceImpl.java
+++ src/main/java/flory/FloryServer/service/TempService/TempCommandServiceImpl.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 //@Service
 //@RequiredArgsConstructor
 //public class TempCommandServiceImpl implements TempQueryService{
-//
+//     // TempRestController 클래스의 생성자에서 필요한 TempQueryService의 빈이 두 개 정의되어 있어 어느 것을 주입할지 모호해서 오류 발생
 //    @Override
 //    public void CheckFlag(Integer flag){
 //
