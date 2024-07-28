c38f5ba94ee11f999ccb1b647a142bc95252ce6d
jisu jang <wltnslagg@naver.com>
2024-07-28 14:47:55 +0900

    Feat: 에러 핸들러 연습

--- src/main/java/flory/FloryServer/converter/TempConverter.java
+++ src/main/java/flory/FloryServer/converter/TempConverter.java
@@ -4,9 +4,15 @@ import flory.FloryServer.web.dto.TempResponse;
 
 public class TempConverter {
 
-    public static TempResponse.TempTestDTO toTempDTO(){
+    public static TempResponse.TempTestDTO toTempTestDTO(){
         return TempResponse.TempTestDTO.builder()
                 .testString("This is Test!!!")
+                .build();
+    }
+
+    public static TempResponse.TempExceptionDTO toTempExceptionDTO(Integer flag){
+        return TempResponse.TempExceptionDTO.builder()
+                .flag(flag)
                 .build();
     }
 }
---
+++ src/main/java/flory/FloryServer/service/TempService/TempCommandServiceImpl.java
@@ -0,0 +1,14 @@
+package flory.FloryServer.service.TempService;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+//@Service
+//@RequiredArgsConstructor
+//public class TempCommandServiceImpl implements TempQueryService{
+//
+//    @Override
+//    public void CheckFlag(Integer flag){
+//
+//    }
+//}
