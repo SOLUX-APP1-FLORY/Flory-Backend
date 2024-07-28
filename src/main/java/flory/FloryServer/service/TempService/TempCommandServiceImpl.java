78d79f47313ea8457bede1b0861a547eed97cb28
ouob123 <kyjin320@sookmyung.ac.kr>
2024-07-29 01:27:47 +0900

    Fix: 응답 통일 코드 수정

--- src/main/java/flory/FloryServer/service/TempService/TempCommandServiceImpl.java
+++ src/main/java/flory/FloryServer/service/TempService/TempCommandServiceImpl.java
@@ -3,12 +3,8 @@ package flory.FloryServer.service.TempService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-//@Service
-//@RequiredArgsConstructor
-//public class TempCommandServiceImpl implements TempQueryService{
-//
-//    @Override
-//    public void CheckFlag(Integer flag){
-//
-//    }
-//}
+@Service
+@RequiredArgsConstructor
+public class TempCommandServiceImpl implements TempCommandService{
+
+}
