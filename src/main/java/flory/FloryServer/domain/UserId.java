1633657e7d026f87f475143c1ae59df016bbf54b
lehiewl <2110801@sookmyung.ac.kr>
2024-07-29 20:47:09 +0900

    [Feat] firebase storage 연결

--- src/main/java/flory/FloryServer/domain/UserId.java
+++ src/main/java/flory/FloryServer/domain/UserId.java
@@ -1,8 +1,30 @@
 package flory.FloryServer.domain;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 public class UserId implements Serializable {
-    /*private Long id;
-    private String nickname;*/
+    private Long id;
+    private String nickname;
+
+    public UserId() {}
+
+    public UserId(Long id, String nickname) {
+        this.id = id;
+        this.nickname = nickname;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserId userId = (UserId) o;
+        return Objects.equals(id, userId.id) &&
+                Objects.equals(nickname, userId.nickname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nickname);
+    }
 }
