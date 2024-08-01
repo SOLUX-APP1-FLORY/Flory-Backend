f52dcdec780d28565204aabeac5d880a6a62c4a6
ouob123 <kyjin320@sookmyung.ac.kr>
2024-08-02 00:59:42 +0900

    Fix: 다이어리 조회 API - token 연동

--- src/main/java/flory/FloryServer/web/dto/DiaryViewResponseDTO.java
+++ src/main/java/flory/FloryServer/web/dto/DiaryViewResponseDTO.java
@@ -15,11 +15,12 @@ public class DiaryViewResponseDTO {
     @AllArgsConstructor
     public static class DiaryViewDetailDTO {
         private Long diary_id;// 다이어리의 고유 ID
+        private Long user_id; // 사용자 ID
         private int flower_id;
         private LocalDateTime created_at;// 일기 작성 날짜
         private String flower_name;// 사용자가 선택한 꽃 이름
         private String flower_meaning;// 사용자가 선택한 감정
-        private String title;
+        private String title; // 일기 제목
         private String content;// 일기 내용
         private String flower_imageUrl;// 선택한 꽃의 사진 URL
     }
