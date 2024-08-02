f99fbddd20cf72dcdbbc4e1718455db3fee9e88e
lehiewl <2110801@sookmyung.ac.kr>
2024-08-03 00:50:22 +0900

    [Fix] webConfig 수정

--- src/main/java/flory/FloryServer/config/SecurityConfig.java
+++ src/main/java/flory/FloryServer/config/SecurityConfig.java
@@ -29,13 +29,13 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)// CSRF 보호 비활성화
-
+        http
+                .csrf(AbstractHttpConfigurer::disable)  // CSRF 보호 비활성화
                 .authorizeHttpRequests(authorizeRequests -> authorizeRequests
-                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
-                        .requestMatchers("/auth/login").permitAll()
-                        .requestMatchers("/api/v1/member").permitAll()
-                        .anyRequest().permitAll()
+                        .requestMatchers("/", "/api/v1/signup", "/api/v1/auth/login","/api/**").authenticated()  // 인증된 사용자만 접근 가능
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()  // Swagger 관련 경로는 모든 사용자 허용
+                        .requestMatchers("/auth/login", "/api/v1/member").permitAll()  // 로그인과 회원가입 경로는 모든 사용자 허용
+                        .anyRequest().permitAll()  // 나머지 모든 경로는 허용
                 )
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)  // Stateless 설정
