fb114e4196beaf9ba712dccfe30dffd7b769ab76
lehiewl <2110801@sookmyung.ac.kr>
2024-08-03 01:52:33 +0900

    [Fix] securityConfig 수정

--- src/main/java/flory/FloryServer/config/WebConfig.java
+++ src/main/java/flory/FloryServer/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://3.37.34.30:8080", "http://localhost:8080")  // 프론트엔드 URL
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
