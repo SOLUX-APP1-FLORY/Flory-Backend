3f53c6f3fdfab87cef46291c3d5a8397e441b737
lehiewl <2110801@sookmyung.ac.kr>
2024-08-03 02:36:42 +0900

    [Fix] securityConfig 수정

--- src/main/java/flory/FloryServer/config/SecurityConfig.java
+++ src/main/java/flory/FloryServer/config/SecurityConfig.java
@@ -76,7 +76,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.addAllowedOrigin("http://3.37.34.30");
+        configuration.addAllowedOrigin("http://3.37.34.30:8080");
         configuration.addAllowedOrigin("http://localhost:8080");
         configuration.addAllowedMethod("*");
         configuration.addAllowedHeader("*");
