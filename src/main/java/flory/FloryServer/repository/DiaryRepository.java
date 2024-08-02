24e481e61d7c85c1d1bed4d749c6d997859890ef
ouob123 <kyjin320@sookmyung.ac.kr>
2024-08-02 16:02:59 +0900

    FIX: 다이어리 조회 API - 작성 시간 -> 작성 날짜로 수정

--- src/main/java/flory/FloryServer/repository/DiaryRepository.java
+++ src/main/java/flory/FloryServer/repository/DiaryRepository.java
@@ -2,8 +2,8 @@ package flory.FloryServer.repository;
 
 import flory.FloryServer.domain.Diary;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
@@ -12,6 +12,8 @@ public interface DiaryRepository extends JpaRepository<Diary, Long> {
     Optional<Diary> findByUserId(Long userId);
     List<Diary> findByUserUid(String uid);
     List<Diary> findByUserUidAndCreatedAtBetween(String uid, LocalDateTime startDate, LocalDateTime endDate);
-    // 사용자 ID와 작성 날짜로 다이어리 조회
-    List<Diary> findAllByUserIdAndCreatedAtBetween(Long userId, LocalDateTime startOfDay, LocalDateTime endOfDay);
+
+    // 사용자 ID와 작성 날짜로 다이어리 조회 (LocalDate로 수정)
+    // List<Diary> findAllByUserIdAndCreatedAtBetween(Long userId, LocalDate startOfDay, LocalDate endOfDay);
+    List<Diary> findAllByUserIdAndCreatedAtBetween(Long userId, LocalDateTime startDate, LocalDateTime endDate);
 }
